1fc5c4e092b902aef6710e7c6d978af4172444f4
Petr Zeman <petr.zeman@digitalcz.cz>
2018-01-18 20:22:32 +0100

    [PPRO] Initial commit, base java springboot application

---
+++ src/main/java/cz/uhk/ppro/semestralniprojekt/SemestralniProjektApplication.java
@@ -0,0 +1,12 @@
+package cz.uhk.ppro.semestralniprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestralniProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestralniProjektApplication.class, args);
+	}
+}
---
+++ src/test/java/cz/uhk/ppro/semestralniprojekt/SemestralniProjektApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.uhk.ppro.semestralniprojekt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SemestralniProjektApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
