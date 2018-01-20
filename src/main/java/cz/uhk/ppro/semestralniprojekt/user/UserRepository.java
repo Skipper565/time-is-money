cce1ab7068432be270e21950c43814bd5013c4f4
Petr Zeman <zemanpe2@uhk.cz>
2018-01-20 20:25:05 +0100

    [PPRO][Broken] Add Oracle DB connection, but fails on foreignKey method.

---
+++ src/main/java/cz/uhk/ppro/semestralniprojekt/user/UserRepository.java
@@ -0,0 +1,11 @@
+package cz.uhk.ppro.semestralniprojekt.user;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByUsername(String username);
+
+}
